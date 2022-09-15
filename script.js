0da04ab0647cf923799dd36e0d16bde471efd443
emmanueljose <emjose@gmail.com>
2022-09-15 13:25:45 -0400

    update html and script

--- script.js
+++ script.js
@@ -620,8 +620,8 @@ function swRegistration() {
 	const heart = ["font-size: 20px", "padding: 12px", "margin: 4px 0 4px 4px", "color: rgba(238,58,136,1)"].join(";");
 	if ("serviceWorker" in navigator) {
 		navigator.serviceWorker
-			.register("/****REPOSITORY-NAME****/sw.js", {
-				scope: "/****REPOSITORY-NAME****/",
+			.register("/kboard/sw.js", {
+				scope: "/kboard/",
 			})
 			.then(function (registration) {
 				console.log("%c❤️", heart);
